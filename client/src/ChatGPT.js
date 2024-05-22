14d084f4be95f848c4e1065d53c28285787eb5bb
andrei <balasandrei200@yahoo.com>
2024-05-22 16:08:21 +0300

    finishing touch

--- client/src/ChatGPT.js
+++ client/src/ChatGPT.js
@@ -1,53 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './ChatGPT.css';
-import Button from '@mui/material/Button';
+import { FaRegUser } from 'react-icons/fa';
+import { RiRobot2Line } from 'react-icons/ri';
 
 export default function ChatGPT() {
-  const [prompt, setPrompt] = useState('how are you');
-  const [response, setResponse] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [responses, setResponses] = useState([]);
+  const [currentResponse, setCurrentResponse] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
   const HTTP = 'http://localhost:8080/chat';
+
+  useEffect(() => {
+    if (currentResponse) {
+      let typingInterval;
+      const typingSpeed = 10; // Milliseconds per character
+      let currentText = '';
+      let index = 0;
+
+      typingInterval = setInterval(() => {
+        currentText += currentResponse.charAt(index);
+        setResponses((prevResponses) =>
+          prevResponses.map((resp, i) =>
+            i === prevResponses.length - 1
+              ? { ...resp, botResponse: currentText }
+              : resp
+          )
+        );
+        index++;
+        if (index === currentResponse.length) {
+          clearInterval(typingInterval);
+        }
+      }, typingSpeed);
+
+      return () => clearInterval(typingInterval);
+    }
+  }, [currentResponse]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post(`${HTTP}`, { prompt })
+      .post(`${HTTP}`, { prompt, conversation: responses }) // Sending conversation history to BE
       .then((res) => {
-        setResponse(res.data);
+        setResponses([...responses, { promptUser: prompt, botResponse: '' }]);
+        setCurrentResponse(res.data);
+        setPrompt('');
       })
       .catch((error) => {
         console.log(error);
       });
-
-    setPrompt('how are you');
   };
 
   const handlePrompt = (e) => {
     setPrompt(e.target.value);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit(event);
+    }
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
-    <div className="container container-sm p-1">
-      <h1 className="title text-center text-darkGreen">ChatGPT API</h1>
-      <form className="form" onSubmit={handleSubmit}>
+    <div className="container ">
+      <div className="title text-center text-darkGreen">
+        Find your Desired Documentaries
+      </div>
+      <div className="chat-container bg-darkGreen mt-2 border-5">
+        {responses.length > 0 &&
+          responses.map((response, index) => (
+            <div key={index} className="response">
+              <div
+                className={`text-light-user textBorder ${
+                  index === responses.length - 1 ? 'animationUser' : ''
+                }`}
+              >
+                <FaRegUser size={50} className="icon" />
+                <p className="userText">{response.promptUser}</p>
+              </div>
+              <div
+                className={`textBorder textBorderResponse text-light-user ${
+                  index === responses.length - 1 ? 'animationBot' : ''
+                }`}
+              >
+                <p className="text-light">{response.botResponse}</p>
+                <RiRobot2Line size={50} className="icon" />
+              </div>
+            </div>
+          ))}
+
         <div className="form-group">
-          <label htmlFor="">Ask questions</label>
-          <input
-            className="shadow-sm"
-            type="text"
-            placeholder="Enter text"
-            value={prompt}
-            onChange={handlePrompt}
-          />
+          <div
+            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
+          >
+            <textarea
+              className="input"
+              placeholder={isFocused ? '' : 'Ask me Anything...'} // Placeholder changes when input is focused
+              value={prompt}
+              onChange={handlePrompt}
+              onKeyDown={handleKeyDown}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              rows={1} // Initial rows
+              style={{
+                width: '50%',
+                borderRadius: '40px',
+                padding: '1rem',
+                resize: 'none', // Prevents manual resizing by the user
+                overflow: 'hidden',
+              }}
+            />
+          </div>
         </div>
-        <Button variant="contained" type="submit">
-          Go
-        </Button>
-      </form>
-      <div className="bg-darkGreen  mt-2 p-1 border-5">
-        <p className="text-light">
-          {response ? response : 'Ask me anything...'}
-        </p>
       </div>
     </div>
   );
--- client/src/server.js
+++ client/src/server.js
@@ -1,37 +1,49 @@
 const OpenAi = require('openai');
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.json());
-app.use(cors());
-// Your OpenAI GPT API key
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
 const OPENAI_API_KEY = process.env.SECRET_API_KEY;
-console.log(OPENAI_API_KEY);
+
 app.post('/chat', async (req, res) => {
   try {
     const openai = new OpenAi({
       apiKey: OPENAI_API_KEY,
     });
+
+    // Extract the prompt and conversation history from the request body
+    const { prompt, conversation } = req.body;
+
+    // Prepare messages array with user prompts and system messages
+    const messages = [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      ...conversation.map(({ promptUser }) => ({
+        role: 'user',
+        content: promptUser,
+      })),
+      { role: 'user', content: prompt },
+    ];
+    // Call the AI model with the extended conversation history
     const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content: 'You are a helpful assistant.',
-        },
-        { role: 'user', content: req.body.prompt },
-      ],
+      messages,
       model: 'gpt-3.5-turbo-0125',
-      response_format: { type: 'json_object' },
     });
-    console.log(completion.choices[0].message.content);
+
+    // Send the response back to the client
+    res.json(completion.choices[0].message.content);
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
